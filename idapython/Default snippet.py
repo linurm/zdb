cb005c589779a70d59e6689aecf13c1d0416bcce
zhj <343600434@qq.com>
2017-01-25 15:05:56 +0800

    i

--- idapython/Default snippet.py
+++ idapython/Default snippet.py
@@ -1,19 +1,48 @@
 def find_addr(addr):
-    while True:
-        addr = idc.PrevHead(addr)
+    #while True:
+    addr = idc.PrevHead(addr)
         #addr = idc.PrevHead(addr)
         #print GetMnem(addr)
         #break
-        if GetMnem(addr) == "MOV" and "R1" in GetOpnd(addr, 0):
-            print "R1 at 0x%x" % addr
-            print GetOperandValue(addr, 1)
+    if GetMnem(addr) == "LDR" and "R0" in GetOpnd(addr, 0):
+        print "at 0x%x" % addr
+        print GetDisasm(addr)# LDR R0,[R2,R6];off_950804
+        print GetOpnd(addr, 0)# R0
+        print GetOpnd(addr, 1)# [R2,R6]
+        print GetOperandValue(addr, 0)# 0
+        print GetOperandValue(addr, 1)# 2
+        type = GetOpType(addr, 0)# 1
+        print type
+        type = GetOpType(addr, 1)# 1
+        print type
+        
+        
+        #if type == 3:
             
-        if 
-            break
+        return 0xffffffff
+    return addr
+        
+            #break
+def getFunction(addr):
+    global cur_addr
+    cur_addr = addr
+    funs = GetFunctionAttr(addr, FUNCATTR_START)
+    #print hex(funs)
+    if (funs != -1):
+        end = GetFunctionAttr(addr, FUNCATTR_END)
+        print hex(funs),'---->',hex(end)
+        start = funs
+        sp_value = GetOperandValue(addr, 1)
+        while addr > start and addr < end :
+            addr = find_addr(addr)
+            #displayCode(start, sp_value)
+    else:
+        Warning("no function found at 0x%x", addr)
+    
 
-
-#find_addr(0x1d5d8)
-
-for addr in XrefsTo(0x001D8D8, flags=0):
-    #print hex(addr.frm)
-    find_addr(addr.frm)
+#find_addr(0x6eaaa0)
+#a = hex(here())
+#print 'HERE ',a
+addr = int(here())
+#addr = 0x6eaaa0
+getFunction(addr)
