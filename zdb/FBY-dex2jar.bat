d831a30e69c5c774d28987ae07cd64ebebaaea7f
zhj <343600434@qq.com>
2017-07-22 16:08:48 +0800

    u

---
+++ zdb/FBY-dex2jar.bat
@@ -0,0 +1,5 @@
+
+cd %~dp0
+echo %cd%
+
+..\dex-tools-2.1-SNAPSHOT\d2j-dex2jar.bat %1
---
+++ zdb/JavaInSmali.py
@@ -0,0 +1,18 @@
+import sys  
+  
+
+for arg in sys.argv:  
+    print arg  
+  
+package_name = raw_input("input package (eg. a.b.b.c): ")
+
+package_name_c = package_name.replace('.','/')
+
+
+class_name = raw_input("input class : ")
+
+#class_name_c = class_name.replace('.','/')
+
+class_function_value = raw_input("input function name: ")
+
+print 'L' + package_name_c + "/" + class_name + ";->"+class_function_value
